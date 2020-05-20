40e870335016e7da70dda1fc161a97b921128b8a
Salathiel Genese <salathielgenese@gmail.com>
2020-05-20 19:55:29 +0400

    feat: add OpenAPI documentation
    I did this following the ProposalController pattern

--- apps/api/src/app/sale-team/sale-team.controller.ts
+++ apps/api/src/app/sale-team/sale-team.controller.ts
@@ -1,15 +1,35 @@
-import { CrudController } from '../core/crud/crud.controller';
-import { Controller, UseGuards } from '@nestjs/common';
+import { Controller, Get, HttpStatus, UseGuards } from '@nestjs/common';
+import { CrudController, IPagination, PaginationParams } from '../core/crud';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { PermissionGuard } from '../shared/guards/auth/permission.guard';
+import { Permissions } from '../shared/decorators/permissions';
 import { SaleTeamService } from './sale-team.service';
+import { PermissionsEnum } from '@gauzy/models';
 import { SaleTeam } from './sale-team.entity';
 import { AuthGuard } from '@nestjs/passport';
-import { ApiTags } from '@nestjs/swagger';
 
 @Controller()
 @ApiTags('Pipeline')
+@UseGuards(PermissionGuard)
 @UseGuards(AuthGuard('jwt'))
 export class SaleTeamController extends CrudController<SaleTeam> {
 	constructor(readonly saleTeamService: SaleTeamService) {
 		super(saleTeamService);
+	}
+
+	@Get()
+	@Permissions(PermissionsEnum.SALES_TEAMS_VIEW)
+	@ApiResponse({
+		isArray: true,
+		type: SaleTeam,
+		status: HttpStatus.OK,
+		description: `Found current user's organization sales teams`
+	})
+	@ApiOperation({
+		summary: `Find all sales teams for current user's organisation.`
+	})
+	public async findAll(): Promise<IPagination<SaleTeam>> {
+		// TODO: Narrow down to user's organization scope
+		return super.findAll();
 	}
 }
